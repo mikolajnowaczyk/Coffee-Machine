21f7713684422b6b6d5a2443889e2fb5035907fc
mikolajnowaczyk <mikolaj35@wp.pl>
2023-03-09 14:48:49 +0100

    Second task

--- Coffee Machine (Java)/task/src/machine/CoffeeMachine.java
+++ Coffee Machine (Java)/task/src/machine/CoffeeMachine.java
@@ -1,7 +1,12 @@
 package machine;
 
+import java.util.Scanner;
+
 public class CoffeeMachine {
-    public static void main(String[] args) {
+    static final int waterPerCup = 200;
+    static final int milkPerCup = 50;
+    static final int coffeeBeansPerCup = 15;
+    private void displayActions(){
         System.out.println("Starting to make a coffee");
         System.out.println("Grinding coffee beans");
         System.out.println("Boiling water");
@@ -9,5 +14,14 @@ public class CoffeeMachine {
         System.out.println("Pouring coffee into the cup");
         System.out.println("Pouring some milk into the cup");
         System.out.println("Coffee is ready!");
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int coffeeDrinks = scanner.nextInt();
+        System.out.println("For "+coffeeDrinks+" cups of caffee you will need:");
+        System.out.println(coffeeDrinks * waterPerCup + " ml of water");
+        System.out.println(coffeeDrinks * milkPerCup+ " ml of milk");
+        System.out.println(coffeeDrinks * coffeeBeansPerCup + " g of coffee beans");
+
     }
 }
